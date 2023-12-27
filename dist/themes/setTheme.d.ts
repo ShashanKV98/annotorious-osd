7ae509de7b26c69aaec24bf0ac63764d812ce1e0
ShashanKV98 <vshashank32@gmail.com>
2023-12-27 03:38:01 -0700

    Delete dist directory

--- dist/themes/setTheme.d.ts
+++
@@ -1,4 +0,0 @@
-import type OpenSeadragon from 'openseadragon';
-import type { Theme } from '@annotorious/annotorious/src';
-export declare const setTheme: (viewer: OpenSeadragon.Viewer, theme: Theme) => void;
-//# sourceMappingURL=setTheme.d.ts.map
