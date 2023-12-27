488ec2d430e714057eb054b73a6f8fd95c116e01
ShashanKV98 <u1265559@utah.edu>
2023-12-27 04:49:09 -0700

    dist

---
+++ dist/themes/setTheme.d.ts
@@ -0,0 +1,4 @@
+import type OpenSeadragon from 'openseadragon';
+import type { Theme } from '@annotorious/annotorious/src';
+export declare const setTheme: (viewer: OpenSeadragon.Viewer, theme: Theme) => void;
+//# sourceMappingURL=setTheme.d.ts.map
